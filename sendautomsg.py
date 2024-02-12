55e2a8f9ed027e5b859b1d76ab6699a07811169d
Bismay Sarangi <142608995+bismaysarangi@users.noreply.github.com>
2024-02-12 16:00:20 +0530

    Update sendautomsg.py

--- sendautomsg.py
+++ sendautomsg.py
@@ -1,3 +1,3 @@
 import pywhatkit as pyw
 
-pyw.sendwhatmsg('+91..........', 'Testing...', 20, 10) #Add the phone number of the receiver
+pyw.sendwhatmsg('+91..........', 'Testing...', 20, 10) #Add the phone number of the receiver, Time in Hours(24Hr Format), Time in Minutes
